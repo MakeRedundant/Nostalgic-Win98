fe64f682c41b657054c3b7802350ef1d072f8b09
MakeRedundant <brian.trang9@gmail.com>
2024-06-14 14:08:08 +1000

    added HeadComponent

---
+++ src/components/HeadComponent/HeadComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const HeadComponent = () => (
+  <>
+    <title>Brian Trang Win 98</title>
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <link rel="stylesheet" href="/css/98.css" />
+    <link rel="stylesheet" href="/css/index.css" />
+    <link rel="stylesheet" href="/css/prog.css" />
+    <link rel="stylesheet" href="/lib/xterm/xterm.css" />
+    <link href="/img/taskbar/WindowsFlagSmall.png" rel="icon" />
+    <meta name="theme-color" content="#008888" />
+    <meta name="name" content="Brian Trang Win 98" />
+    <meta name="description" content="System Admin &amp; IT Consultant." />
+    <meta property="og:title" content="Brian Trang Win 98" />
+    <meta property="og:type" content="website" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:description" content="System Admin &amp; IT Consultant." />
+    <meta property="og:url" content="https://info.alicankismet.com" />
+    <meta property="og:image" content="https://info.alicankismet.com/preview.png" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://info.alicankismet.com" />
+    <meta property="twitter:title" content="Brian Trang Win 98" />
+    <meta property="twitter:description" content="System Admin &amp; IT Consultant." />
+    <meta property="twitter:image" content="https://info.alicankismet.com/preview.png" />
+  </>
+);
+
+export default HeadComponent;
